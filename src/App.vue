bbebc6629515a6f6c2bb7820165a4d2ee68ad30e
Michelle PF <michellepadillafranco@gmail.com>
2020-07-13 12:48:26 -0400

    usando multiples componentes vue

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <MiLista titulo="Lenguajes Aprendidos:" v-bind:enlaces="lenguajes_aprendidos"/>
+    <MiLista titulo="Lenguajes por Aprender:" v-bind:enlaces="lenguajes_por_aprender"/>
+  </div>
+</template>
+
+<script>
+import MiLista from './components/MiLista.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MiLista
+  },
+   data: function(){
+        return{
+            lenguajes_aprendidos: [
+              {texto:'HTML', url:"https://developer.mozilla.org/es/docs/HTML/HTML5"}, 
+              {texto:'CSS', url:"https://desarrolloweb.com/manuales/css3.html"},
+              {texto:'Vue', url:"https://vuejs.org/"},
+            ],
+            lenguajes_por_aprender: [
+              {texto:'Python', url:"https://www.python.org/"}, 
+              {texto:'Dart', url:"https://dart.dev/"},
+              {texto:'React', url:"https://es.reactjs.org/"},
+            ]
+        }
+       
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MiLista.vue
@@ -0,0 +1,44 @@
+.<template>
+  <div class="wrap">
+    <div id="miApp">
+        <h3>{{titulo}}</h3>
+        <div>
+            <ul>
+               <li v-for="(enlace, i) in enlaces" v-bind:key="i"><a v-bind:href="enlace.url">{{enlace.texto}}</a></li>
+            </ul>
+        </div>
+    </div>
+    
+   
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MiLista',
+    props: {
+        titulo: String,
+        enlaces: Array
+    }
+   
+
+}
+</script>
+
+<style scoped>
+    .wrap {
+    margin: auto;
+    height: auto;
+    width: 800px;
+    text-align: center;
+    border: 5px solid crimson;
+    font-size: 25px;
+    background-color: coral;
+    color: white;
+    }
+    ul {
+        list-style: none;
+    }
+
+
+</style>
